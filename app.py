9ae3f11ff3bbe73fd7e3d6c4eed6dc2fbcf13858
Keerti Lata Choudhury <161821127+Keertilata20@users.noreply.github.com>
2026-03-02 12:50:27 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,2 +1,21 @@
+from flask import Flask, render_template, request, jsonify
+from risk_engine import calculate_risk
 
-print("Zero Trust Auth Backend")
+app = Flask(__name__)
+
+@app.route('/')
+def dashboard():
+    return render_template('dashboard.html')
+
+@app.route('/behavior', methods=['POST'])
+def behavior():
+    data = request.json
+    keystrokes = data.get("keystrokes", [])
+    mouse = data.get("mouse", [])
+
+    risk = calculate_risk(keystrokes, mouse)
+
+    return jsonify({"risk": risk})
+
+if __name__ == "__main__":
+    app.run(debug=True)
